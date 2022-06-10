47d791fe9383ab8f14ebae54ddc60053d55ed829
abdurrahmankucuk90 <abdurrahmankucuk90@gmail.com>
2022-06-10 12:14:46 +0200

    advance calc is published

--- calc.js
+++ calc.js
@@ -1,163 +1,241 @@
-const calculator = document.querySelector(".calculator");
-const numb = document.querySelector(".numb");
-const yellow = document.querySelector(".yellow");
-const ac = document.querySelector(".ac");
-const screen2 = document.querySelector(".d1");
-const screen1 = document.querySelector(".d2");
-const eq = document.querySelector(".eq");
-const np = document.querySelector(".np");
-screen2.innerHTML = [];
 const arr1 = [];
 const arr2 = [];
 const arr3 = [];
+const arr4 = [];
+const screen2 = document.querySelector(".d1");
+const screen1 = document.querySelector(".d2");
+
+const calculator = document.querySelector(".calculator");
+let a = 0;
+let b = 0;
+let c = 0;
+//*use reduce
 
 calculator.addEventListener("click", (e) => {
-  if (arr2.length == 0) {
-    //*Number input
+  //!Main if
+  if (c == 0) {
+    //!Numbers except zero
     if (e.target.classList.contains("numb")) {
       arr1.push(e.target.innerText);
       screen1.innerText = arr1.join("");
-      if (arr1[0] == 0 && arr1[1] != "" && arr1[1] != ".") {
+      if (arr1[0] == 0 && arr1[1] != ".") {
         arr1.splice(0, 1);
-        // arr1.push(e.target.innerText);
         screen1.innerText = arr1.join("");
       }
-    } //* zero input
+    }
+    //! Zero
     else if (e.target.classList.contains("zero")) {
-      if (arr1.length < 1) {
+      if (arr1.length == 0) {
         arr1.push(e.target.innerText);
         screen1.innerText = arr1.join("");
-      } else if (arr1.length == 2) {
-        if (arr1[0] == 0 && arr1[1] == ["."]) {
-          arr1.push(e.target.innerText);
+      } else if (arr1.length == 1) {
+        if (arr1[0] == 0) {
           screen1.innerText = arr1.join("");
         } else {
           arr1.push(e.target.innerText);
           screen1.innerText = arr1.join("");
         }
-      } else if (arr1.length == 1 || arr1.length > 2) {
-        if (arr1.length == 1) {
-          screen1.innerText = arr1.join("");
-        } else {
-          if (arr1[0] == 0) {
-            arr1.push(e.target.innerText);
-            screen1.innerText = arr1.join("");
-          }
-        }
-      }
-      
-    } //*decimal input
-    else if (e.target.classList.contains("dot")) {
-      if (!arr1.includes(".")) {
+      } else if (arr1.length >= 2) {
         arr1.push(e.target.innerText);
+        screen1.innerText = arr1.join("");
       }
-      if (arr1[0] == ".") {
-        arr1.splice(0, 0, "0");
-      }
-      screen1.innerText = arr1.join("");
-    } //*operation input
-    else if (e.target.classList.contains("yellow")) {
-      arr2.push(e.target.innerText);
-      screen2.innerText = arr1.join("") + " " + arr2.join("") + " ";
-      screen1.innerText = "";
-    } //*reset button
+    }
+    //!Delete icon
     else if (e.target.classList.contains("ac")) {
       arr1.splice(0, arr1.length);
       arr2.splice(0, arr2.length);
-      arr3.splice(0, arr3.length);
       screen1.innerText = "";
       screen2.innerText = "";
-    } //*negatif input
+    }
+    //!Decimel icon
+    else if (e.target.classList.contains("dot")) {
+      if (arr1.length > 0) {
+        if (arr1.includes(".")) {
+          screen1.innerText = arr1.join("");
+        } else {
+          arr1.push(".");
+          screen1.innerText = arr1.join("");
+        }
+      } else if (arr1.length == 0) {
+        arr1.push("0");
+        arr1.push(".");
+        screen1.innerText = arr1.join("");
+      }
+    }
+    //!Prosent icon
+    else if (e.target.classList.contains("ps")) {
+      let d = arr1.reduce((a, b) => a + b) / 100;
+      arr1.splice(0, arr1.length, d);
+      screen1.innerText = arr1.join("");
+      let a = parseFloat(arr1.join(""));
+    }
+    //!Negative icon
     else if (e.target.classList.contains("np")) {
-      if (arr1[0] != "-") {
+      if (arr1.includes("-")) {
+        screen1.innerText = arr1.join("");
+      } else {
         arr1.splice(0, 0, "-");
         screen1.innerText = arr1.join("");
       }
     }
-    //* yuzde input
-    else if (e.target.classList.contains("ps")) {
-      screen1.innerText = screen1.innerText / 100;
-      arr1.splice(0, arr1.length, ...screen1.innerText);
-      console.log(arr1);
+    //!Operation icon
+    else if (e.target.classList.contains("yellow")) {
+      if (arr1.length == 0 && arr2.length == 0) {
+        arr1.push("0");
+        screen1.innerText = arr1.join("");
+        arr2.push(e.target.innerText);
+        screen2.innerText = arr1.join("") + " " + arr2.join("") + " ";
+        a = parseFloat(arr1.join(""));
+        arr1.splice(0, arr1.length);
+        screen1.innerText = "";
+      } else if (arr2.length > 0) {
+        arr2.splice(1, arr2.length);
+      } else {
+        arr2.push(e.target.innerText);
+        screen2.innerText = arr1.join("") + " " + arr2.join("") + " ";
+        a = parseFloat(arr1.join(""));
+        arr1.splice(0, arr1.length);
+        screen1.innerText = "";
+      }
     }
-  } //*arr3 starts from here
-  //*number input for arr3
+    //!After operation entred
+    else if (e.target.classList.contains("numb") && arr2[0] != "") {
+      arr1.push(e.target.innerText);
+      screen1.innerText = arr1.join("");
+    }
+    //!First operation run
+    else if (e.target.classList.contains("eq")) {
+      b += parseFloat(arr1.join(""));
+      arr4.push(a);
+      arr4.push(b);
+      if (arr2[0] == "+") {
+        c = arr4.reduce((a, b) => a + b);
+        arr2.splice(0, arr2.length);
+      } else if (arr2[0] == "X") {
+        c = arr4.reduce((a, b) => a * b);
+        arr2.splice(0, arr2.length);
+      } else if (arr2[0] == "/") {
+        c = arr4.reduce((a, b) => a / b);
+        arr2.splice(0, arr2.length);
+      } else if (arr2[0] == "-") {
+        c = arr4.reduce((a, b) => a - b);
+        arr2.splice(0, arr2.length);
+      }
+      arr4.splice(0, arr4.length, c);
+      screen2.innerText = arr4.join("");
+      arr1.splice(0, arr1.length);
+      screen1.innerText = "";
+    }
+  }
+
+  //!Main else
   else {
-    if (e.target.classList.contains("numb")) {
-      arr3.push(e.target.innerText);
-      screen1.innerText = arr3.join("");
-      if (arr3[0] == 0 && arr3[1] != "" && arr3[1] != ".") {
-        arr3.splice(0, 1);
-        // arr3.push(e.target.innerText);
-        screen1.innerText = arr3.join("");
+    //!Operation icon
+    if (e.target.classList.contains("yellow")) {
+      if (arr1.length == 0 && arr2.length == 0) {
+        arr1.push("0");
+        screen1.innerText = arr1.join("");
+        arr2.push(e.target.innerText);
+        screen2.innerText = c + " " + arr2.join("") + " ";
+        arr1.splice(0, arr1.length);
+        screen1.innerText = "";
+      } else if (arr2.length > 0) {
+        arr2.splice(1, arr2.length);
+      } else {
+        arr2.push(e.target.innerText);
+        screen2.innerText = c + " " + arr2.join("") + " ";
+        arr1.splice(0, arr1.length);
+        screen1.innerText = "";
       }
-    } //*zero input for arr3
-    else if (e.target.classList.contains("zero")) {
-      if (arr3.length < 1) {
-        arr3.push(e.target.innerText);
-        screen1.innerText = arr3.join("");
-      } else if (arr3.length == 2) {
-        if (arr3[0] == 0 && arr3[1] == ["."]) {
-          arr3.push(e.target.innerText);
-          screen1.innerText = arr3.join("");
-        }
-      } else if (arr3.length == 1 || arr3.length > 2) {
-        if (arr3.length == 1) {
-          screen1.innerText = arr3.join("");
-        } else {
-          if (arr3[0] == 0) {
-            arr3.push(e.target.innerText);
-            screen1.innerText = arr3.join("");
-          }
-        }
-      }
-    } //* yuzde for arr3
-    else if (e.target.classList.contains("ps")) {
-      screen1.innerText = screen1.innerText / 100;
-      arr3.splice(0, arr3.length, ...screen1.innerText);
-      console.log(arr3);
     }
-    //*Negatif input for arr3
+    //!Negative icon
     else if (e.target.classList.contains("np")) {
-      if (arr3[0] != "-") {
-        arr3.splice(0, 0, "-");
-        screen1.innerText = arr3.join("");
+      if (arr1.includes("-")) {
+        screen1.innerText = arr1.join("");
+      } else {
+        arr1.splice(0, 0, "-");
+        screen1.innerText = arr1.join("");
       }
-    } //*decimal input for arr3
-    else if (e.target.classList.contains("dot")) {
-      if (!arr3.includes(".")) {
-        arr3.push(e.target.innerText);
+    }
+    //!Number
+    else if (e.target.classList.contains("numb")) {
+      arr1.push(e.target.innerText);
+      screen1.innerText = arr1.join("");
+      if (arr1[0] == 0 && arr1[1] != ".") {
+        arr1.splice(0, 1);
+        screen1.innerText = arr1.join("");
       }
-      if (arr3[0] == ".") {
-        arr3.splice(0, 0, "0");
-      }
-      screen1.innerText = arr3.join("");
-    } //*reset button for arr3
+    } else if (e.target.classList.contains("ps")) {
+      let d = arr1.reduce((a, b) => a + b) / 100;
+      arr1.splice(0, arr1.length, d);
+      screen1.innerText = arr1.join("");
+      let c = parseFloat(arr1.join(""));
+    }
+    //!Delete icon
     else if (e.target.classList.contains("ac")) {
       arr1.splice(0, arr1.length);
       arr2.splice(0, arr2.length);
-      arr3.splice(0, arr3.length);
+      arr4.splice(0, arr4.length);
       screen1.innerText = "";
       screen2.innerText = "";
-    } else if (e.target.classList.contains("eq")) {
-      screen2.innerText = calc(+arr1.join(""), arr2[0], +arr3.join(""));
+      a = 0;
+      b = 0;
+      c = 0;
+    }
+    //!Decimel icon
+    else if (e.target.classList.contains("dot")) {
+      if (arr1.length > 0) {
+        if (arr1.includes(".")) {
+          screen1.innerText = arr1.join("");
+        } else {
+          arr1.push(".");
+          screen1.innerText = arr1.join("");
+        }
+      } else if (arr1.length == 0) {
+        arr1.push("0");
+        arr1.push(".");
+        screen1.innerText = arr1.join("");
+      }
+    }
+    //! Zero
+    else if (e.target.classList.contains("zero")) {
+      if (arr1.length == 0) {
+        arr1.push(e.target.innerText);
+        screen1.innerText = arr1.join("");
+      } else if (arr1.length == 1) {
+        if (arr1[0] == 0) {
+          screen1.innerText = arr1.join("");
+        } else {
+          arr1.push(e.target.innerText);
+          screen1.innerText = arr1.join("");
+        }
+      } else if (arr1.length >= 2) {
+        arr1.push(e.target.innerText);
+        screen1.innerText = arr1.join("");
+      }
+    }
+
+    //!Continous Operation run
+    else if (e.target.classList.contains("eq")) {
+      b = parseFloat(arr1.join(""));
+      arr4.push(b);
+      if (arr2[0] == "+") {
+        c = arr4.reduce((a, b) => a + b);
+        arr2.splice(0, arr2.length);
+      } else if (arr2[0] == "X") {
+        c = arr4.reduce((a, b) => a * b);
+        arr2.splice(0, arr2.length);
+      } else if (arr2[0] == "/") {
+        c = arr4.reduce((a, b) => a / b);
+        arr2.splice(0, arr2.length);
+      } else if (arr2[0] == "-") {
+        c = arr4.reduce((a, b) => a - b);
+        arr2.splice(0, arr2.length);
+      }
+      arr4.splice(0, arr4.length, c);
+      screen2.innerText = arr4.join("");
       arr1.splice(0, arr1.length);
-      arr2.splice(0, arr2.length);
-      arr3.splice(0, arr3.length);
       screen1.innerText = "";
     }
-    console.log(arr1);
   }
 });
-
-function calc(a, b, c) {
-  if (b == "+") {
-    return a + c;
-  } else if (b == "-") {
-    return a - c;
-  } else if (b == "/") {
-    return a / c;
-  } else if (b == "X") {
-    return a * c;
-  }
-}
---
+++ calca1.js
@@ -0,0 +1,163 @@
+const calculator = document.querySelector(".calculator");
+const numb = document.querySelector(".numb");
+const yellow = document.querySelector(".yellow");
+const ac = document.querySelector(".ac");
+const screen2 = document.querySelector(".d1");
+const screen1 = document.querySelector(".d2");
+const eq = document.querySelector(".eq");
+const np = document.querySelector(".np");
+screen2.innerHTML = [];
+const arr1 = [];
+const arr2 = [];
+const arr3 = [];
+
+calculator.addEventListener("click", (e) => {
+  if (arr2.length == 0) {
+    //*Number input
+    if (e.target.classList.contains("numb")) {
+      arr1.push(e.target.innerText);
+      screen1.innerText = arr1.join("");
+      if (arr1[0] == 0 && arr1[1] != "" && arr1[1] != ".") {
+        arr1.splice(0, 1);
+        // arr1.push(e.target.innerText);
+        screen1.innerText = arr1.join("");
+      }
+    } //* zero input
+    else if (e.target.classList.contains("zero")) {
+      if (arr1.length < 1) {
+        arr1.push(e.target.innerText);
+        screen1.innerText = arr1.join("");
+      } else if (arr1.length == 2) {
+        if (arr1[0] == 0 && arr1[1] == ["."]) {
+          arr1.push(e.target.innerText);
+          screen1.innerText = arr1.join("");
+        } else {
+          arr1.push(e.target.innerText);
+          screen1.innerText = arr1.join("");
+        }
+      } else if (arr1.length == 1 || arr1.length > 2) {
+        if (arr1.length == 1) {
+          screen1.innerText = arr1.join("");
+        } else {
+          if (arr1[0] == 0) {
+            arr1.push(e.target.innerText);
+            screen1.innerText = arr1.join("");
+          }
+        }
+      }
+      
+    } //*decimal input
+    else if (e.target.classList.contains("dot")) {
+      if (!arr1.includes(".")) {
+        arr1.push(e.target.innerText);
+      }
+      if (arr1[0] == ".") {
+        arr1.splice(0, 0, "0");
+      }
+      screen1.innerText = arr1.join("");
+    } //*operation input
+    else if (e.target.classList.contains("yellow")) {
+      arr2.push(e.target.innerText);
+      screen2.innerText = arr1.join("") + " " + arr2.join("") + " ";
+      screen1.innerText = "";
+    } //*reset button
+    else if (e.target.classList.contains("ac")) {
+      arr1.splice(0, arr1.length);
+      arr2.splice(0, arr2.length);
+      arr3.splice(0, arr3.length);
+      screen1.innerText = "";
+      screen2.innerText = "";
+    } //*negatif input
+    else if (e.target.classList.contains("np")) {
+      if (arr1[0] != "-") {
+        arr1.splice(0, 0, "-");
+        screen1.innerText = arr1.join("");
+      }
+    }
+    //* yuzde input
+    else if (e.target.classList.contains("ps")) {
+      screen1.innerText = screen1.innerText / 100;
+      arr1.splice(0, arr1.length, ...screen1.innerText);
+      console.log(arr1);
+    }
+  } //*arr3 starts from here
+  //*number input for arr3
+  else {
+    if (e.target.classList.contains("numb")) {
+      arr3.push(e.target.innerText);
+      screen1.innerText = arr3.join("");
+      if (arr3[0] == 0 && arr3[1] != "" && arr3[1] != ".") {
+        arr3.splice(0, 1);
+        // arr3.push(e.target.innerText);
+        screen1.innerText = arr3.join("");
+      }
+    } //*zero input for arr3
+    else if (e.target.classList.contains("zero")) {
+      if (arr3.length < 1) {
+        arr3.push(e.target.innerText);
+        screen1.innerText = arr3.join("");
+      } else if (arr3.length == 2) {
+        if (arr3[0] == 0 && arr3[1] == ["."]) {
+          arr3.push(e.target.innerText);
+          screen1.innerText = arr3.join("");
+        }
+      } else if (arr3.length == 1 || arr3.length > 2) {
+        if (arr3.length == 1) {
+          screen1.innerText = arr3.join("");
+        } else {
+          if (arr3[0] == 0) {
+            arr3.push(e.target.innerText);
+            screen1.innerText = arr3.join("");
+          }
+        }
+      }
+    } //* yuzde for arr3
+    else if (e.target.classList.contains("ps")) {
+      screen1.innerText = screen1.innerText / 100;
+      arr3.splice(0, arr3.length, ...screen1.innerText);
+      console.log(arr3);
+    }
+    //*Negatif input for arr3
+    else if (e.target.classList.contains("np")) {
+      if (arr3[0] != "-") {
+        arr3.splice(0, 0, "-");
+        screen1.innerText = arr3.join("");
+      }
+    } //*decimal input for arr3
+    else if (e.target.classList.contains("dot")) {
+      if (!arr3.includes(".")) {
+        arr3.push(e.target.innerText);
+      }
+      if (arr3[0] == ".") {
+        arr3.splice(0, 0, "0");
+      }
+      screen1.innerText = arr3.join("");
+    } //*reset button for arr3
+    else if (e.target.classList.contains("ac")) {
+      arr1.splice(0, arr1.length);
+      arr2.splice(0, arr2.length);
+      arr3.splice(0, arr3.length);
+      screen1.innerText = "";
+      screen2.innerText = "";
+    } else if (e.target.classList.contains("eq")) {
+      screen2.innerText = calc(+arr1.join(""), arr2[0], +arr3.join(""));
+      arr1.splice(0, arr1.length);
+      arr2.splice(0, arr2.length);
+      arr3.splice(0, arr3.length);
+      screen1.innerText = "";
+    }
+    console.log(arr1);
+  }
+});
+
+function calc(a, b, c) {
+  if (b == "+") {
+    return a + c;
+  } else if (b == "-") {
+    return a - c;
+  } else if (b == "/") {
+    return a / c;
+  } else if (b == "X") {
+    return a * c;
+  }
+}
